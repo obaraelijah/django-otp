86f555e179c5bbb6f81a43844c2a2030e2ef8959
obaraelijah <elijahobara357@gmail.com>
2023-08-20 18:12:28 +0300

    test: fix code quality

--- app/user/tests/confest.py
+++ app/user/tests/confest.py
@@ -1,2 +1,12 @@
 def api_client_with_credentials(token: str, api_client):
+    """
+    This function sets the HTTP_AUTHORIZATION header in the given API client with the provided token.
+    
+    Args:
+        token (str): The authentication token to be included in the authorization header.
+        api_client: The API client instance on which the authorization header will be set.
+        
+    Returns:
+        api_client: The modified API client instance with the authorization header set.
+    """
     return api_client.credentials(HTTP_AUTHORIZATION="Bearer " + token)
